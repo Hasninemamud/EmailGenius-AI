23476d3ced215ab1ca0c8cf09b911df2fc333682
Hasnine Mamud <hasninehemel85@gmail.com>
2025-02-20 23:10:52 +0600

    add vercel file

--- app.py
+++ app.py
@@ -78,4 +78,6 @@ def generate():
     return jsonify({"generated_text": formatted_text})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
